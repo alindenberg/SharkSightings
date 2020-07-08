b3459e4a8fc985e6e034bb7f8076f3533edfa0df
Austin Lindenberg <alindenberg8@gmail.com>
2020-07-08 06:42:52 -0700

    Format error json

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import express from "express"
 
 import winstonLogger, * as logger from './config/logger'
 import { initDb } from './config/database'
-import jwtCheck from './config/auth'
 import sightingsRouter from "./routes/sightings"
 
 dotenv.config();
@@ -17,7 +16,6 @@ app.use(cors())
 app.use(express.json())
 app.use(winstonLogger);
 
-app.use(jwtCheck)
 app.use(sightingsRouter)
 
 app.listen(port, () => {
--- src/routes/sightings.ts
+++ src/routes/sightings.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import jwtCheck from '../config/auth'
 import SightingService from '../services/sightings'
 
 const router = express.Router();
@@ -7,31 +8,31 @@ const service = new SightingService();
 router.get('/sightings', async (req, res, next) => {
     await service.getAllSightings(req)
         .then(sightings => res.status(200).send(sightings))
-        .catch(err => res.status(400).send(err.message));
+        .catch(err => res.status(400).send(format_error(err)));
 })
 
 router.get('/sightings/:id', async (req, res, next) => {
     await service.getSighting(req.params.id)
         .then(sighting => res.status(200).send(sighting))
-        .catch(err => res.status(400).send(err.message))
+        .catch(err => res.status(400).send(format_error(err)))
 })
 
-router.post('/sightings', async (req: any, res, next) => {
+router.post('/sightings', jwtCheck, async (req: any, res, next) => {
     await service.createSighting(req.body, req.user.sub)
         .then((sighting) => res.status(200).send(sighting))
-        .catch(err => res.status(400).send(err.message));
+        .catch(err => res.status(400).send(format_error(err)));
 })
 
-router.put('/sightings/:id', async (req, res, next) => {
+router.put('/sightings/:id', jwtCheck, async (req, res, next) => {
     await service.updateSighting(req.params.id, req.body)
         .then(() => res.sendStatus(200))
-        .catch(err => res.status(400).send(err.message))
+        .catch(err => res.status(400).send(format_error(err)))
 })
 
-router.delete('/sightings/:id', async (req: any, res, next) => {
+router.delete('/sightings/:id', jwtCheck, async (req: any, res, next) => {
     await service.deleteSighting(req.params.id, req.user.sub)
         .then(() => res.sendStatus(204))
-        .catch(err => res.status(400).send(err.message))
+        .catch(err => res.status(400).send(format_error(err)))
 })
 
 router.get("/api/external", (req: any, res) => {
@@ -39,4 +40,7 @@ router.get("/api/external", (req: any, res) => {
     res.send({ msg: "Access token validated." })
 })
 
+function format_error(err: Error) {
+    return { error: err.message }
+}
 export default router;
